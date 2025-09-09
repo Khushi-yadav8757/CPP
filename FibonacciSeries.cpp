1525923576f98777e8a72e77418fe6c2fa390d51
Khushi Yadav <kky957242@gmail.com>
2025-09-09 21:16:09 +0530

    Create FibonacciSeries.cpp

---
+++ FibonacciSeries.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n = 10, t1 = 0, t2 = 1, next;
+    cout << "Fibonacci: ";
+    for (int i = 1; i <= n; i++) {
+        cout << t1 << " ";
+        next = t1 + t2;
+        t1 = t2;
+        t2 = next;
+    }
+    return 0;
+}
